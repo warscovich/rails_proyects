18df1a3b02405b11d049fc76c809db72fcb87b1a
warscovich <warscovich@gmail.com>
2013-08-17 17:32:59 -0500

    commit incial
    commit de proyectos

---
+++ PesomaticApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PesomaticApp::Application.config.secret_key_base = '2863824e2e8d9d2b363ad8d2af67d6800b550b9f2f53852e2967ea561011aace64fca0735e1cc1f8b968854d4d0d8c31b393765558457db9c2ef7d425b7a7f2e'
---
+++ PesomaticApp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PesomaticApp::Application.config.session_store :cookie_store, key: '_PesomaticApp_session'
