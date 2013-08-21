4d9cd453df07a03e5d3c77ea71258b2592d40738
warscovich <warscovich@gmail.com>
2013-08-21 04:01:50 -0500

    Comit Final

--- PesomaticApp/features/Pesomatic.feature
+++ PesomaticApp/features/Pesomatic.feature
@@ -6,3 +6,10 @@ Scenario: Calcular su peso BMI para una persona de 72 pulgadas
   When I fill in "height" with my height which is 72 inches
   And  I press "GetWeight" button
   Then I should see my ideal weight (BMI) in pounds or kilograms
+  
+Scenario: Calcular su peso devine para una persona de 72 pulgadas
+
+  Given I am in the Pesomatic weight calculator page
+  When I fill in "height" with my height which is 72 inches
+  And  I press "GetWeight" button
+  Then I should see "170.72" men, "160.84" women 
--- PesomaticApp/features/step_definitions/Pesomatic_steps.rb
+++ PesomaticApp/features/step_definitions/Pesomatic_steps.rb
@@ -3,6 +3,7 @@ Given(/^I am in the Pesomatic weight calculator page$/) do
 end
 
 When(/^I fill in "(.*?)" with my height which is (\d+) inches$/) do |height_element, inches|
+  page.select('Pulgadas', :from => 'system')
   fill_in height_element, with: inches
 end
 
@@ -14,3 +15,11 @@ Then(/^I should see my ideal weight \(BMI\) in pounds or kilograms$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+Then(/^I should see "(.*?)" men, "(.*?)" women$/) do |ideal_weight1, ideal_weight2|
+   
+  #expect(page).to have_content ideal_weight1
+  #page.should(have_content ideal_weight1)
+  #page.should(have_content ideal_weight2)
+ find("label", :text => ideal_weight1)
+ find("label", :text => ideal_weight2)
+end
