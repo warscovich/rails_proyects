df11bcdbd7c7539d67fc95a0b654fb7fab77bfe4
warscovich <warscovich@gmail.com>
2013-08-20 23:52:11 -0500

    Commit! con nuevo ruteo

--- PesomaticApp/app/controllers/calcular_peso_controller.rb
+++ PesomaticApp/app/controllers/calcular_peso_controller.rb
@@ -1,6 +1,12 @@
 class CalcularPesoController < ApplicationController
   def index
     #@altura = 0
+    @formulas = Hash.new
+  end
+  
+  def calcular_peso_bmi
+    @formulas["bmi"] = (()*(@height * @height))/703
+    #@system
   end
   
   def libras_a_kilos libras
